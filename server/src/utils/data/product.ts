d15fda1e2e44bf4e107d7d3ef940d374c5f462ed
anano303 <124179773+anano303@users.noreply.github.com>
2025-01-25 19:22:35 +0400

    Initial commit

---
+++ server/src/utils/data/product.ts
@@ -0,0 +1,12 @@
+import { url } from "inspector";
+
+export const sampleProduct = {
+  name: "Sample name",
+  price: 0,
+  image: url,
+  brand: "Sample brand",
+  category: "Sample category",
+  numReviews: 0,
+  countInStock: 0,
+  description: "Sample description",
+};
